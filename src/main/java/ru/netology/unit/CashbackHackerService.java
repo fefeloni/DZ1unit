5d6fad65edec9d068e4b191ac5467ee00e52154d
fefeloni <fefeloni-t@mail.ru>
2022-09-10 11:42:07 +0500

    second

--- src/main/java/ru/netology/unit/CashbackHackerService.java
+++
@@ -1,9 +0,0 @@
-package ru.netology.unit;
-
-public class CashbackHackerService {
-    private final int boundary = 1000;
-
-    public int remain(int amount) {
-        return boundary - amount % boundary;
-    }
-}
