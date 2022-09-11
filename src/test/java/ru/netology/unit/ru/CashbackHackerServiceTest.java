1fd74f7a2684e30544402036653179af37ab1c04
fefeloni <fefeloni-t@mail.ru>
2022-09-12 01:00:33 +0500

    first commit

--- src/test/java/ru/netology/unit/ru/CashbackHackerServiceTest.java
+++ src/test/java/ru/netology/unit/ru/CashbackHackerServiceTest.java
@@ -1,37 +1,37 @@
 package ru.netology.unit.ru;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackerServiceTest {
     @Test
-    void shouldCashbackHackerBelow() {
+    public void shouldCashbackHackerBelow() {
         CashbackHackerService cashbackHackerService = new CashbackHackerService();
-        int amount = 100;
-        int expected = 900;
-        int actual = cashbackHackerService.remain(amount);
-
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    void  shouldCashbackHackerAbove() {
-        CashbackHackerService cashbackHackerService = new CashbackHackerService();
-        int amount = 1500;
+        int amount = 500;
         int expected = 500;
         int actual = cashbackHackerService.remain(amount);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void  shouldCashbackHackerAbove() {
+        CashbackHackerService cashbackHackerService = new CashbackHackerService();
+        int amount = 2500;
+        int expected = 500;
+        int actual = cashbackHackerService.remain(amount);
+
+        assertEquals(expected, actual);
     }
 
     //@Test
     //void shouldCashbackHacker() {
-        //CashbackHackerService cashbackHackerService = new CashbackHackerService();
-        //int amount = 1000;
-        //int expected = 0; тест падает в этом месте
-        //int actual = cashbackHackerService.remain(amount);
+    //CashbackHackerService cashbackHackerService = new CashbackHackerService();
+    //int amount = 1000;
+    //int expected = 0; тест падает в этом месте
+    //int actual = cashbackHackerService.remain(amount);
 
-        //assertEquals(actual, expected);
-   // }
+    //assertEquals(actual, expected);
+    // }
 }
